0c4ea167801bfae532a6dd4a0fa535711a9f8875
Andrzej Barabasz <abarabasz@gmail.com>
2025-03-13 22:43:01 +0100

    update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 **Drakula** is a theme for [Visual Studio Code](https://github.com/microsoft/vscode) that combines the things I like most from the [Dracula](https://github.com/dracula/visual-studio-code) and [OneDark](https://github.com/Binaryify/OneDark-Pro) themes.
 
+## Recommendations
+
+* Font: [JetBrains Mono](https://www.jetbrains.com/lp/mono/) ([Nerd Fonts](https://www.nerdfonts.com/font-downloads) variant)
---
+++ scripts/generate.js
@@ -0,0 +1,22 @@
+const { readFile } = require('fs').promises;
+const { join } = require('path');
+const { Type, DEFAULT_SCHEMA, load } = require('js-yaml');
+
+module.exports = async () => {
+    const yamlFile = await readFile(
+        join(__dirname, '..', 'src', 'drakula.yml'),
+        'utf-8'
+    );
+
+    const base = load(yamlFile, { schema });
+
+    for (const key of Object.keys(base.colors)) {
+        if (!base.colors[key]) {
+            delete base.colors[key];
+        }
+    }
+
+    return {
+        base
+    };
+};
